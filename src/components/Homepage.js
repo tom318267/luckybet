12c91fa1386263536cc092acdd230e5f7cfcd338
Thomas Rago <thomasrago@Thomass-Air.fios-router.home>
2021-07-22 13:41:49 -0400

    adjust homepage img size

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,12 +4,12 @@ import luckybet from "../assets/luckybet.svg";
 const Homepage = () => {
   return (
     <div className="w-screen h-screen bg-black flex flex-col justify-center items-center border-b-2">
-      <img className="w-1/2 object-contain" src={luckybet} alt="luckybet" />
+      <img className="w-3/4 object-contain" src={luckybet} alt="luckybet" />
       <div>
-        <button className="p-2 px-6 bg-red mr-2 sm:mr-4 rounded-full sm:text-lg text-white text-xs font-race">
+        <button className="p-2 px-6 bg-red mr-2 sm:mr-4 rounded-full text-lg text-white font-race">
           Sign Up
         </button>
-        <button className="py-2 px-8 bg-red rounded-full sm:text-lg text-white text-xs font-race">
+        <button className="py-2 px-8 bg-red rounded-full sm:text-lg text-white text-lg font-race">
           Login
         </button>
       </div>
