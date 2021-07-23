c0f804d14e8bd7b9a0a9ab962c507af1d82237fb
Thomas Rago <thomasrago@Thomass-Air.fios-router.home>
2021-07-23 18:46:22 -0400

    adjus t form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,12 +3,12 @@ import React from "react";
 const SignUp = () => {
   return (
     <div className="bg-red flex justify-center items-center w-screen h-screen overflow-hidden">
-      <div className="w-4/5 h-1/2 flex bg-white">
+      <div className="w-4/5 h-2/3 rounded-lg flex bg-white">
         <form className="flex flex-col justify-center items-center">
-          <h2 className="text-2xl text-center font-race mb-4">
+          <h2 className="text-3xl text-center font-race mb-4">
             Create an Account
           </h2>
-          <div className="w-4/5 font-electro text-sm">
+          <div className="w-4/5 font-electro text-lg">
             <label>Name</label>
             <input className="border w-full mb-1" type="text" />
             <label>Email Address</label>
@@ -16,10 +16,10 @@ const SignUp = () => {
             <label>Password</label>
             <input className="border w-full mb-1" type="text" />
           </div>
-          <button className="p-2 bg-red text-sm text-white font-race w-4/5 mb-4">
+          <button className="p-2 bg-red text-lg text-white font-race w-4/5 mb-4">
             Register
           </button>
-          <p className="text-sm">
+          <p className="text-sm font-electro">
             Already have an account? <strong className="text-red">Login</strong>
           </p>
         </form>
